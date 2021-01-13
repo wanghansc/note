cc6fbebb18e68c89d1bcaca8fc3b5ca4cf542479
王瀚 <1561675255@qq.com>
2021-01-13 09:33:28 +0800

    实体类对象设置

--- src/main/java/com/wh/searchall/pojo/Type.java
+++ src/main/java/com/wh/searchall/pojo/Type.java
@@ -1,9 +1,57 @@
 package com.wh.searchall.pojo;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author wanghan
  * @description TODO
  * @date 2021/1/12 10:14
  **/
+@Entity
+@Table(name = "t_type")
 public class Type {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "type")
+    private List<Blog> blogs = new ArrayList<>();
+
+    public Type() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Blog> getBlogs() {
+        return blogs;
+    }
+
+    public void setBlogs(List<Blog> blogs) {
+        this.blogs = blogs;
+    }
+
+    @Override
+    public String toString() {
+        return "Type{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
